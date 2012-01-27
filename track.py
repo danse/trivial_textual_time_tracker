7a114f1f989ff44df6583b7e8ccfc250e1c39419
Francesco <francesco@civilization.(none)>
2012-01-27 23:09:44 +0100

    switched from futures to threads (present also in python 3.1), added file logging and interval prompting

--- track.py
+++ track.py
@@ -1,24 +1,61 @@
 import sys
 import time
 import datetime
-import concurrent.futures
+import threading
 
+default = ''
 continue_ = True
 
 def points(seconds=60):
     while continue_:
-        time.sleep(seconds)
+        for s in range(seconds):
+            if continue_: time.sleep(1)
         print('.', end='')
         sys.stdout.flush()
 
-def background(*args):
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-    future   = executor.submit(*args)
-    return future
+def prompt(now, interval=None):
+    '''
+    Start waiting a new description from the user. Show to the user a new slot
+    where to write the description of current task.
+    '''
+    if interval: print(interval)
+    print()
+    print(now)
+
+class FileManager:
+    def __init__(self, argv):
+        try:
+            position  = default or argv[1]
+            self.file = open(position, 'a')
+        except Exception as e:
+            print('Caught exception "{0}", not writing the log to the filesystem'.format(e))
+            self.file = None
+
+    def new_record(self, number, string):
+        if self.file:
+            record = self.format_record(number, string)
+            self.file.write(record)
+
+    @staticmethod
+    def format_record(number, string):
+        '''
+        >>> format_record(33, 'a long description')
+        33, a long description
+        '''
+        return '{0}, {1}'.format(number, string)
 
 if __name__=='__main__':
-    background(points)
+    f=FileManager(sys.argv)
+    t=threading.Thread(None, points)
+    t.start()
+    now = datetime.datetime.now()
+    prompt(now)
     for line in sys.stdin:
-        print()
-        print(datetime.datetime.now())
+        before   = now
+        now      = datetime.datetime.now()
+        interval = now - before
+        interval = round(interval.seconds/60)
+        f.new_record(interval, line)
+        prompt(now, interval)
     continue_ = False
+    print('Please wait while our pointer is finishing pointing...')
