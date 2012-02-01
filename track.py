f7bc08193069877f14cd8c3b0102e6fbafd1d73c
francesco <francesco@mercurio.(none)>
2012-02-01 17:35:46 +0100

    bugfix

--- track.py
+++ track.py
@@ -1,9 +1,10 @@
+import re
 import sys
 import time
 import datetime
 import threading
 
-default = '/home/francesco/repos/trivial_textual_time_tracker/var/log'
+default = '/home/francesco/repos/trivial_textual_time_tracker/log'
 continue_ = True
 
 def points(seconds=60):
@@ -34,16 +35,18 @@ class FileManager:
     def new_record(self, *args):
         if self.file:
             record = self.format_record(*args)
-            self.file.write(record)
+            self.file.write('{0}\n'.format(record))
+            self.file.flush()
 
     @staticmethod
-    def format_record(*args):
+    def format_record(number, description, date=None):
         '''
-        >>> FileManager.format_record(33, 'a long description')
-        '33, a long description'
+        >>> FileManager.format_record(33, 'a #long description\\n', datetime.datetime(2012, 2, 1, 16, 41, 59, 369339))
+        '33, a #long description, 2012-02-01 16:41:59.369339'
         '''
-        args = map(str, args)
-        return ', '.join(args)
+        description = re.sub('\n', '', description)
+        fields      = map(str, (number, description, date))
+        return ', '.join(fields)
 
 if __name__=='__main__':
     f=FileManager(sys.argv)
